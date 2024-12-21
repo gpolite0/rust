812add89ab3c2d5e6779fb370ad3057b50e85e76
Gabrielle Polite <gpolite@stanford.edu>
2024-12-21 02:25:47 +0000

    edit comments

--- trash/src/main.rs
+++ trash/src/main.rs
@@ -9,17 +9,21 @@ fn main() {
     println!("{c}, {z}, {heart_eyed_cat}");
 
 
-    // tuples
+    // tuples - can be diff types
     let tuple = (1, 2, 3.4);
     println!("Tuple element 0: {}", tuple.0);
     println!("Tuple element 1: {}", tuple.1);
     println!("Tuple element 2: {}", tuple.2);
 
-    // arrays - fixed size, allocated on heap
+
+    // arrays - fixed size, all same type, allocated on stack
     let array = ["Jan", "Feb", "Mar"];
     let array_size = array.len();
-    
+
     for i in 0..array_size {
         println!("Array at index {i}: {}", array[i]);
     }
+
+    
+    // vectors - variable size, all same type, allocated in memory
 }
