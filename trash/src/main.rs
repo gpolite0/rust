9c166cbe63cc43b5ac04b540bc43da65672d2ebd
Gabrielle Polite <gpolite@stanford.edu>
2024-12-21 02:14:24 +0000

    print tuples

--- trash/src/main.rs
+++ trash/src/main.rs
@@ -1,7 +1,17 @@
+
 fn main() {
+
+    // chars
     let c = 'z';
     let z: char = 'ℤ'; // with explicit type annotation
     let heart_eyed_cat = '😻';  // can print emojis!!! uses ASCII
 
     println!("{c}, {z}, {heart_eyed_cat}");
+
+
+    // tuples
+    let tuple = (1, 2, 3.4);
+    println!("Tuple element 0: {}", tuple.0);
+    println!("Tuple element 1: {}", tuple.1);
+    println!("Tuple element 2: {}", tuple.2);
 }
